49c583dc8ba82b7a1b3dd722f66c6bb3b7b4f5bb
Nikolay <niktyler7@gmail.com>
2022-05-11 11:59:38 +0300

    Работа с состоянием. Добавление сотрудника в избранное.

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -6,7 +6,8 @@ class EmployeesListItem extends Component {
     constructor (props) {
         super(props);
         this.state = {
-          increase: false
+          increase: false,
+          star: false
         }
     }
 
@@ -16,20 +17,35 @@ class EmployeesListItem extends Component {
         }))
     }
 
+    onLike = () => {
+        this.setState(({star}) => ({
+            star: !star
+        }))
+    }
+
 
     render() {
         const {name, salary} = this.props;
-        const {increase} = this.state;
+        const {increase, star} = this.state;
 
 
         let classNames = "list-group-item d-flex justify-content-between";
         if (increase) {
             classNames += ' increase';
-        }   
+        }
+
+        if (star) {
+            classNames += ' like';
+        }
+
+        // function classToggle() {
+
+        // }
     
         return (
             <li className={classNames}>
-                <span className="list-group-item-label">{name}</span>
+                <span className="list-group-item-label"
+                onClick={this.onLike}>{name}</span>
                 <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"
