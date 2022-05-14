163ee120319a36215dcd3a46797c9350b486a076
Nikolay <niktyler7@gmail.com>
2022-05-14 13:31:30 +0300

    Добавление нового сотрудника. Передача state на верхний уровень.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -19,6 +19,7 @@ class App extends Component {
                 {name: 'Alex M.', salary: 5000, increase: false, id: 3}
             ]
         }
+        this.maxId = 4;
     }
 
     deleteItem = (id) => {
@@ -31,6 +32,22 @@ class App extends Component {
 
             return {
                 data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    addItem = (name, salary) => {
+
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState (({data}) => {
+            const newArr = [...data, newItem]
+            return {
+                data: newArr
             }
         })
     }
@@ -48,7 +65,7 @@ class App extends Component {
                 <EmployeesList 
                 data={this.state.data}
                 onDelete={this.deleteItem}/>
-                <EmployeesAddForm/>
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
     }
