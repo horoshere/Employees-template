59cc0ba2835d6026989ff87410318bee558b2444
Nikolay <niktyler7@gmail.com>
2022-05-16 15:02:59 +0300

    Подъем состояния. Перенос state в App. Рефакторинг list-item. Отображение в app-info.

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,12 +1,13 @@
 
 import './app-info.css'
 
-const AppInfo = () => {
+const AppInfo = ({lengthEmployees, increased}) => {
+    
     return (
         <div className="app-info">
             <h1>Учет сотрудников в компании horoshere Inc</h1>
-            <h2>Общее число сотрудников: </h2>
-            <h2>Премию получат: </h2>
+            <h2>Общее число сотрудников: {lengthEmployees} </h2>
+            <h2>Премию получат: {increased}</h2>
         </div>
     );
 }
