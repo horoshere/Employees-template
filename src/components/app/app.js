32f6d48fb4c07d5ec0c13cf4c510739619357682
Nikolay <niktyler7@gmail.com>
2022-05-16 20:16:03 +0300

    Создан метод для фильтра сотрудников. Еще не прокинут на нижний уровень.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ class App extends Component {
                 {name: 'Carl J.', salary: 800, increase: false, rise: true, id: 1},
                 {name: 'Danny W.', salary: 3000, increase: true, rise: false, id: 2},
                 {name: 'Alex M.', salary: 5000, increase: false, rise: false, id: 3}
-            ]
+            ],
+            term: ''
         }
         this.maxId = 4;
     }
@@ -76,13 +77,21 @@ class App extends Component {
         }))
     }
 
-    onToggleRise = (id) => {
-        console.log(`Rise this ${id}`);
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1;
+        })
     }
 
     render() {
         const lengthEmployees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const {data, term} = this.state;
+        const visibleData = this.searchEmp(data, term);
         return (
             <div className="app">
                 <AppInfo lengthEmployees={lengthEmployees}
@@ -94,7 +103,7 @@ class App extends Component {
                 </div>
     
                 <EmployeesList 
-                data={this.state.data}
+                data={visibleData}
                 onDelete={this.deleteItem}
                 onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
