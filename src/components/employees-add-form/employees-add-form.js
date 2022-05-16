580cb794f6e41f91dbe1333c0db72f3deee5d20c
Nikolay <niktyler7@gmail.com>
2022-05-16 15:33:07 +0300

    Создана небольшая валидация для Добавления нового сотрудника.

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -19,16 +19,27 @@ class EmployeesAddForm extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
-
-        this.props.onAdd(this.state.name, this.state.salary);
-        this.setState({
-            name: '',
-            salary:''
-        })
+        
+        if (this.state.name.length > 2 && this.state.salary !=='') {
+            this.props.onAdd(this.state.name, this.state.salary);
+            this.setState({
+                name: '',
+                salary:''
+            })
+        }  else {
+            this.setState({
+                error: true
+            })
+        }
     }
 
     render() {
-        const{name, salary} = this.state;
+        const{name, salary, error} = this.state;
+        let classNames = 'form-control new-post-label';
+ 
+        if (error) {
+            classNames += ' red';
+        }
         return (
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
@@ -36,13 +47,13 @@ class EmployeesAddForm extends Component {
                     className="add-form d-flex"
                     onSubmit={this.onSubmit}>
                     <input type="text"
-                        className="form-control new-post-label"
+                        className={classNames}
                         placeholder="Как его зовут?"
                         name="name" 
                         value={name}
                         onChange={this.onValueChange}/>
                     <input type="number"
-                        className="form-control new-post-label"
+                        className={classNames}
                         placeholder="З/П в $?" 
                         name="salary"
                         value={salary}
